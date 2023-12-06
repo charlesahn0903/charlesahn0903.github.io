8f44651cdd1014c82b6e59adbdee65e9f1909fdf
Charles JH Ahn <63956441+charlesahn0903@users.noreply.github.com>
2023-12-07 03:12:41 +0900

    기본정보 추가 및 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
-  language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  title: `Charles JH Ahn`,
+  description: ``,
+  language: `ko`
+  siteUrl: `https://charlesahn0903.github.io`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,15 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `안진형`,
     bio: {
       role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      description: ['변하지 않는 꿈과', '무한한 열정으로', '쉬지 않고 도전하는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/charlesahn0903`, 
+      email: `charlesahn0903@gmail.com`, 
     },
   },
 
@@ -43,13 +42,21 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
+        date: '2021.11.',
+        activity: '2021 대한민국 인재상 수상 (부총리 겸 교육부장관상)',
+        links: {
+          news: 'https://www.ulsanpress.net/news/articleView.html?idxno=390655',
+          maazine: 'https://www.hani.co.kr/arti/society/schooling/1040240.html'
+        }
+      },
+      {
+        date: '2023.12 ~',
         activity: '개인 블로그 개발 및 운영',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
-        },
+          //post: '/gatsby-starter-zoomkoding-introduction',
+          //github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          //demo: 'https://www.zoomkoding.com',
+        }
       },
     ],
 
@@ -72,15 +79,15 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
-        description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
-        links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+         title: 'Will be updated!',
+        // description:
+        //   '',
+        // techStack: ['', ''],
+        // thumbnailUrl: 'blog.png',
+        // links: {
+        // post: '/gatsby-starter-zoomkoding-introduction',
+        // github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+        // demo: 'https://www.zoomkoding.com',
         },
       },
     ],
