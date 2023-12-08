6f8c0289b4f33a9bb060101c00e54860266719cd
charles903 <charles0903@naver.com>
2023-12-09 07:05:34 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0022e2fe0340f7fe59cb.js"
+    "url": "webpack-runtime-2de6abecea3a2f351e0b.js"
   },
   {
-    "url": "styles.9a7db5df8aad8aa08c7f.css"
+    "url": "styles.a7bbd76cf8854db4d002.css"
   },
   {
     "url": "framework-de157c6b30d4790e4e81.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "765553bcb3c0b40916aa539c4ffb77cf"
+    "revision": "b2369005aa199313c3bd303fc6684829"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
